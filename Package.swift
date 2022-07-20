d15457600d139f8423822112ecf4fbdfeebee88d
viktor.volkov <viktor.volkov@e-legion.com>
2022-07-20 17:02:19 +0300

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+    name: "ContextMenuSwift",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(name: "ContextMenuSwift", targets: ["ContextMenuSwift"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "ContextMenuSwift",
+            dependencies: [],
+            path: "ContextMenuSwift",
+            exclude: ["Info.plist"]
+        )
+    ]
+)
